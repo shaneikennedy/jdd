50fb402bc2d294d0bb4c8572277117652e983d5a
Shane Kennedy <shane.kennedy19@gmail.com>
2021-03-27 18:12:14 -0400

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,29 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-netlify-cms`,
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-0EBX5QT741", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+        },
+      },
+    },
+    {
       resolve: `gatsby-plugin-google-adsense`,
       options: {
         publisherId: `ca-pub-8404355088424689`
